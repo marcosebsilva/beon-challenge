081930b49da2f00001ed0ecfd83951e9a1da67b0
Marcos Estevao <dusdoom.dev@gmail.com>
2022-06-21 20:08:54 -0300

    style: added PageController styling

--- src/components/PageController/index.tsx
+++ src/components/PageController/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, ChangeEvent, useState } from 'react';
 import useBooks from '../../context/BooksContext';
 import useSearch from '../../context/SearchContext';
 import * as Styled from './style';
@@ -9,18 +9,52 @@ interface ActivePageObject {
 export default function PageController() {
   const { totalCount } = useBooks();
   const { limit, updateSearch } = useSearch();
+  const [showError, setShowError] = useState<boolean>(false);
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const numberOfPages = useMemo(() => Math.ceil(totalCount / limit), [totalCount, limit]);
 
-  const numberOfPages = useMemo(() => Math.ceil(totalCount / limit), [totalCount]);
   const handlePageClick = ({ selected }: ActivePageObject) => {
     updateSearch({ page: selected + 1 });
   };
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newLimit = Number(e.target.value);
+    if (newLimit < 5) {
+      setShowError(true);
+      setErrorMsg('Quantidade mínima permitida é 5.');
+      updateSearch({ limit: 5 });
+    } else if (newLimit > 50) {
+      setShowError(true);
+      setErrorMsg('Quantidade máxima permitida é 50.');
+      updateSearch({ limit: 50 });
+    } else {
+      setErrorMsg('');
+      setShowError(false);
+      updateSearch({ limit: newLimit });
+    }
+  };
+
   return (
-    <Styled.Pagination
-      pageCount={numberOfPages}
-      marginPagesDisplayed={1}
-      pageRangeDisplayed={2}
-      onPageChange={handlePageClick}
-    />
+    <Styled.Wrapper>
+      <Styled.Pagination
+        pageCount={numberOfPages}
+        marginPagesDisplayed={1}
+        previousClassName="previousButton"
+        nextClassName="nextButton"
+        pageRangeDisplayed={2}
+        onPageChange={handlePageClick}
+      />
+      <Styled.Label htmlFor="page-size-controller">
+        Itens por pagina:
+        {' '}
+        <input
+          id="page-size-controller"
+          type="number"
+          placeholder="10"
+          onChange={handleChange}
+        />
+      </Styled.Label>
+      {showError && <Styled.ErrorMsg>{errorMsg}</Styled.ErrorMsg>}
+    </Styled.Wrapper>
   );
 }
--- src/components/PageController/style.ts
+++ src/components/PageController/style.ts
@@ -1,27 +1,54 @@
-/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 import ReactPaginate from 'react-paginate';
 
 export const Pagination = styled(ReactPaginate)`
+  width: 100%;
+  max-width: 500px;
+  margin: auto;
   display: flex;
+  padding-left: 0;
+  justify-content: center;
+  border: 0.5px solid white;
+  border-radius: 5px;
+  background-color: #CBC3E3;
+
   li {
     cursor: pointer;
+    text-align: center;
     padding: 5px;
+    width: 100px;
     height: 100%;
-    border: 0.5px solid rgba(0, 0, 0, 0.2);
     &:hover {
       background-color: purple;
+      color: white;
     }
   }
 
-  li:first-child {
-    border-radius: 5px 0 0 5px;
-  }
-
-  li:last-child{
-    border-radius: 0 5px 5px 0;
-  }
   .selected {
+    color: white;
     background: purple;
   }
+`;
+
+export const Wrapper = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  justify-content: center;
+`;
+
+export const Label = styled.label`
+  display: inline;
+  input {
+    background-color: white;
+    max-width: 30px;
+    text-align: center;
+    border-radius: 3px;
+  }
+`;
+
+export const ErrorMsg = styled.span`
+  color: red;
 `;
