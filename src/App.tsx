32cbfcc553c47c6418900c5325ff0eec3d49fd8c
Marcos Estevao <dusdoom.dev@gmail.com>
2022-06-22 00:11:15 -0300

    ci: updated tsconfig and cypress config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import BookTable from './components/BookTable';
 import Header from './components/Header';
-import PageController from './components/PaginationController';
+import PageController from './components/PageController';
+import useBooks from './context/BooksContext';
+import useSearch from './context/SearchContext';
+import { getAllBooks, getBooksByQuery } from './api/calls';
 
 function App() {
+  const { updateBooks, books, totalCount } = useBooks();
+  const options = useSearch();
+
+  const handleUpdateOptions = useCallback(async () => {
+    try {
+      const { updateSearch, ...optionsWithoutUpdateFunction } = options;
+      if (options.q || options.year_gte !== undefined || options.year_lte !== undefined) {
+        const result = await getBooksByQuery(optionsWithoutUpdateFunction);
+        updateBooks(result);
+      } else {
+        const result = await getAllBooks(optionsWithoutUpdateFunction);
+        updateBooks(result);
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }, [options]);
+
+  useEffect(() => {
+    handleUpdateOptions();
+  }, [options]);
   return (
     <>
-      <Header />
-      <BookTable />
-      <PageController />
+      <Header updateSearch={options.updateSearch} totalCount={totalCount} />
+      <BookTable books={books} />
+      <PageController
+        limit={options.limit}
+        page={options.page}
+        totalCount={totalCount}
+        updateSearch={options.updateSearch}
+      />
     </>
   );
 }
